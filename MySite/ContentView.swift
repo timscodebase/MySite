43b794e2d5617889c74298fee0409e5269685981
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-06-21 17:19:02 -0700

    Initial Commit

---
+++ MySite/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  MySite
+//
+//  Created by Timothy Smith on 6/21/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySite/MySiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySiteApp.swift
+//  MySite
+//
+//  Created by Timothy Smith on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MySiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
